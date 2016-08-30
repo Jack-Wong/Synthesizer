b5f0524e7e97ef152babfbf333b3ed0e02a83daa
Jack-Wong <jack.wongg@hotmail.com>
2016-08-30 00:47:59 -0400

    Add notes reducer and root reducer

---
+++ frontend/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import notes from './notes_reducer';
+
+
+const reducer = combineReducers({
+  notes: notes
+});
+
+export default reducer;
---
+++ frontend/reducers/notes_reducer.js
@@ -0,0 +1,45 @@
+import NotesConstants from './actions/note_actions';
+import NOTE_NAMES from './util/tones';
+
+const validKeys = ["a", "s", "d", "f", "g"];
+const keyMap = {
+  'a': 'C5',
+  's': 'D5',
+  'd': 'E5',
+  'f': 'F5',
+  'g': 'G5'
+}
+
+const notes = (state = [], action => {
+  const idx = state.indexOf(action.key);
+  switch(action.type) {
+    case "KEY_PRESSED":
+      if(validKeys.indexOf(action.key) === -1){
+        return state;
+      }
+      if(idx === -1) {
+        return [
+          ...state,
+          action.key
+        ];
+      } else {
+        return state;
+      }
+    case "KEY_RELEASED":
+      if(validKeys.indexOf(action.key) === -1){
+        return state;
+      }
+      if(idx !== -1) {
+        return [
+          ...state.slice(0,idx),
+          ...state.slice(idx + 1)
+        ];
+      } else {
+        return state;
+      }
+    default:
+      return state;
+  }
+});
+
+export default notes;
