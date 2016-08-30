086c46f7a5750d79d0a39558871586c6a69bcaf7
AaronCCWong <aarn.wong@gmail.com>
2016-08-29 22:49:35 -0400

    First commit

---
+++ frontend/synthesizer.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<h1>hi</h1>, document.getElementById('root'));
+});
