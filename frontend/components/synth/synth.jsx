774d85cddd5452fe104079387409aff2ca72f0c7
Jack-Wong <jack.wongg@hotmail.com>
2016-09-02 13:35:23 -0400

    Add keydown and keyup event listeners

--- frontend/components/synth/synth.jsx
+++ frontend/components/synth/synth.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { NOTE_NAMES } from '../../util/tones';
+import { TONES } from '../../util/tones';
+import Note from '../../util/note';
+import $ from 'jquery';
+
 
 class Synth extends React.Component {
+  constructor(props){
+    super(props);
+    this.notes = NOTE_NAMES.map(note => {
+      const newNote = new Note(TONES[note]);
+      newNote.name = note;
+      return newNote;
+    });
+  };
+
+  onKeyDown(event) {
+    var keyName = event.key;
+    this.props.keyPressed(keyName);
+  };
+
+  onKeyUp(event) {
+    var keyName = event.key;
+    this.props.keyReleased(keyName);
+  };
+
+  componentDidMount() {
+    $(document).on('keydown', event => onKeyDown(event));
+    $(document).on('keyup', event => onKeyUp(event));
+  };
+
   render() {
+    console.log(this.notes);
+    const displayNotes = this.notes.map((note, index) => {
+      return <li key={index}>{note.name}</li>
+    });
+
     return(
-      <div>Synth</div>
+      <ul>
+        {displayNotes}
+      </ul>
     );
   }
 };
